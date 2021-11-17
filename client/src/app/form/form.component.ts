196e691bea15be7be561ec0df0ae03fb1100a699
TheJuniorAde <adeildo.mesquita@gmail.com>
2021-11-17 14:30:08 -0300

    Add client

---
+++ client/src/app/form/form.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export type ApiResponse<T> = {
+  data: T;
+  success: boolean;
+  error?: any;
+};
+
+export interface TodoEntity {
+  id: number;
+  name: string;
+  status: 'pending' | 'finished';
+}
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent {
+  constructor(
+    public dialogRef: MatDialogRef<FormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: TodoEntity
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
---
+++ client/src/app/httpClient/index.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { environment } from 'src/environments/environment';
+
+export interface RequestConfig extends AxiosRequestConfig {
+  host: string;
+}
+
+function setHeaders(existingHeaders: any) {
+  const headers = {
+    Accept: 'application/json',
+  };
+
+  return {
+    ...existingHeaders,
+    ...headers,
+  };
+}
+
+function buildConfig(config: RequestConfig): AxiosRequestConfig {
+  const { host, ...axiosConfig } = config;
+
+  axiosConfig.baseURL = `${environment.apiUrl}`;
+  axiosConfig.headers = setHeaders(axiosConfig.headers);
+
+  return axiosConfig;
+}
+
+export default function api(config: RequestConfig) {
+  return axios.create(buildConfig(config));
+}
